4431cf77f685369217eaecd0ae4e353d54ebbbd8
watcharapong50 <mmxx15566@hotmail.com>
2018-12-17 14:43:34 +0700

    Add files via upload

--- SERVER/models/system.model.js
+++ SERVER/models/system.model.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 var systemSchema = new mongoose.Schema({
-    timeDelay: { type: Number, required: false, default: "30" },
+    timeDelay: { type: Number, required: false, default: 30 },
+    bathPerNum: { type: Number, required: false, default: 4 },
 });
 
 mongoose.model('System', systemSchema);
